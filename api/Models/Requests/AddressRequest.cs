12e8ed1cfd23786fad6c4bb52ee54af059b2b64e
khartson <khartson2017@gmail.com>
2025-11-30 17:17:41 -0500

    feat(requests): add validation attributes for AddressRequest parameters

--- api/Models/Requests/AddressRequest.cs
+++ api/Models/Requests/AddressRequest.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Models.Requests
 {
     public record AddressRequest(
         string Street,
         string City,
+        [param: Required]
+        [param: StringLength(2, MinimumLength = 2, ErrorMessage = "State must be in 2 letter abbreviation")]
         string State,
         string Zip
     );
 }
+
+// A note on [param: ] vs [propert: ] annotation syntax 
+/*
+    param: applies the attribute to the parameter of the primary constructor.
+    property: applies the attribute to the generated property.
+
+    In this case, using [param: ] is appropriate because we want to validate the input 
+    parameters when creating an instance of AddressRequest. If we used [property: ],
+    the validation would apply to the properties after the object is constructed, 
+    resulting in a 500 Internal Server Error instead of a 400 Bad Request when invalid data is provided.
+*/
